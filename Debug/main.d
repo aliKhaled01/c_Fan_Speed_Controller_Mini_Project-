ec471f21e5172bf37d9407c789c7735b5f6251e4
aliKhale01 <alimanpro2@gmail.com>
2022-09-17 17:16:13 +0200

    fan speed control

---
+++ Debug/main.d
@@ -0,0 +1,12 @@
+main.o main.o: ../main.c ../lcd.h ../std_types.h ../adc.h ../motor.h \
+  ../common_macros.h
+
+../lcd.h:
+
+../std_types.h:
+
+../adc.h:
+
+../motor.h:
+
+../common_macros.h:
---
+++ Debug/motor.d
@@ -0,0 +1,9 @@
+motor.o motor.o: ../motor.c ../pwm.h ../std_types.h ../motor.h ../gpio.h
+
+../pwm.h:
+
+../std_types.h:
+
+../motor.h:
+
+../gpio.h:
